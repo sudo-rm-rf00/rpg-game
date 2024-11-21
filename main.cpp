a5d9a829289b83d09cfea2338414fb8fc32c9899
karmater <mrctrr97@gmail.com>
2024-11-21 11:10:05 +0100

    Added bullets
    -Added a function that normalize the bullet speed and direction
    -Changed resolution from 1600x900 to 1920x1080
    -Bullets can be fired with the click of the left mouse button

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <math.h>
+
+sf::Vector2f NormalizeVector(sf::Vector2f vector)
+{
+    float m = std::sqrt(vector.x * vector.x + vector.y * vector.y);
+
+    sf::Vector2f normalizedVector;
+    normalizedVector.x = vector.x / m;
+    normalizedVector.y = vector.y / m;
+
+    return normalizedVector;
+}
 
 int main()
 {
@@ -7,9 +19,13 @@ int main()
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    sf::RenderWindow window(sf::VideoMode(1600, 900), "RPG Game", sf::Style::Default, settings);
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "RPG Game", sf::Style::Default, settings);
     
     //-------------------INITIALIZE----------------
+
+    std::vector<sf::RectangleShape> bullets;
+
+    float bulletSpeed = 2.0f;
 
 
     //-------------------LOAD----------------
@@ -21,7 +37,8 @@ int main()
     {
         std::cout << "Skeleton texture loaded!\n";
         skeletonSprite.setTexture(skeletonTexture);
-        skeletonSprite.setPosition(sf::Vector2f(400, 100));
+        skeletonSprite.scale(sf::Vector2f(3, 3));
+        skeletonSprite.setPosition(sf::Vector2f(400, 700));
 
         int XIndex = 0;
         int YIndex = 2;
@@ -40,6 +57,8 @@ int main()
     {
         std::cout << "Player texture loaded!\n";
         playerSprite.setTexture(playerTexture);
+        playerSprite.scale(sf::Vector2f(3, 3));
+        playerSprite.setPosition(sf::Vector2f(1650, 800));
 
         int XIndex = 0;
         int YIndex = 0;
@@ -51,7 +70,6 @@ int main()
     //-------------------Player----------------
     //-------------------LOAD----------------
 
-
     // main game loop
     while (window.isOpen())
     {
@@ -62,6 +80,8 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        //bullet.setPosition(bullet.getPosition() + bulletDirection * bulletSpeed);
 
         sf::Vector2f position = playerSprite.getPosition();
 
@@ -76,6 +96,20 @@ int main()
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
             playerSprite.setPosition(position + sf::Vector2f(0, -1));
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        {
+            bullets.push_back(sf::RectangleShape(sf::Vector2f(50, 25)));
+            int i = bullets.size() - 1;
+            bullets[i].setPosition(playerSprite.getPosition());
+        }
+
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            sf::Vector2f bulletDirection = skeletonSprite.getPosition() - bullets[i].getPosition();
+            bulletDirection = NormalizeVector(bulletDirection);
+            bullets[i].setPosition(bullets[i].getPosition() + bulletDirection * bulletSpeed);
+        }
         //-------------------UPDATE----------------
 
 
@@ -83,6 +117,10 @@ int main()
         window.clear(sf::Color::Black);
         window.draw(playerSprite);
         window.draw(skeletonSprite);
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            window.draw(bullets[i]);
+        }
         window.display();
         //-------------------DRAW----------------
     }
